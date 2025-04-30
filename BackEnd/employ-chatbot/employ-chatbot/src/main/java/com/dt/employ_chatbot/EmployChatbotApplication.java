a36b58513989b247ca4ff960310da1beb596f1b5
kivccc <71814049+kivccc@users.noreply.github.com>
2025-04-30 18:30:30 +0900

    Spring Initalize

---
+++ BackEnd/employ-chatbot/employ-chatbot/src/main/java/com/dt/employ_chatbot/EmployChatbotApplication.java
@@ -0,0 +1,14 @@
+package com.dt.employ_chatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployChatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployChatbotApplication.class, args);
+		System.out.println("Hello");
+	}
+
+}
---
+++ BackEnd/employ-chatbot/employ-chatbot/src/test/java/com/dt/employ_chatbot/EmployChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dt.employ_chatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployChatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
