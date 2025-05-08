22953c9fb8426c49ec060e390694c08a6074671f
kivccc <71814049+kivccc@users.noreply.github.com>
2025-05-09 01:55:27 +0900

    minor : react router 매핑 수정

--- BackEnd/employ-chatbot/employ-chatbot/src/main/java/com/dt/employ_chatbot/controller/FrontendController.java
+++ BackEnd/employ-chatbot/employ-chatbot/src/main/java/com/dt/employ_chatbot/controller/FrontendController.java
@@ -1,14 +1,14 @@
 package com.dt.employ_chatbot.controller;
 
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class FrontendController {
-
-    @RequestMapping(value = { "/", "/{path:^(?!api|static|favicon\\.ico|logo192\\.png|manifest\\.json$).*$}" })
-    public String forward() {
-        return "/index.html";
+public class FrontendController implements ErrorController {
+    @GetMapping({"/", "/error"})
+    public String index() {
+        return "index.html";
     }
 }
 
